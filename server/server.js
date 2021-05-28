19914895a23dfe73e0edecd3e5d90f611fc82018
neharajesh <leehanseok3@gmail.com>
2021-05-29 01:46:37 +0530

    ADD: Frontend Authentication

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const authRouter = require("./routers/auth-router")
 const userRouter = require("./routers/user-router")
 
 app.all('/*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", 'POST, OPTIONS',  "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
